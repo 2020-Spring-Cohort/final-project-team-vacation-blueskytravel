3afae59d383eaa0da6f6a821e1af2088b109c34b
dealer426 <burnssamuel01@gmail.com>
2020-03-31 12:06:26 -0400

    Added vote and for fun model with testing

---
+++ BlueSkyTravel/BlueSkyTravel.Tests/ForFunModelTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using BlueSkyTravel.Models;
+
+namespace BlueSkyTravel.Tests
+{
+    public class ForFunModelTests
+    {
+        ForFun myForFun = new ForFun();
+
+        public ForFunModelTests()
+        {
+            myForFun = new ForFun(1, DateTime.Today, 45.00, false, "Orlando", "Didny Worl");
+        }
+
+        [Fact]
+        public void ForFunConstructor_Sets_Id_On_Model()
+        {
+            var result = myForFun.Id;
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ForFunConstructor_Sets_Fare_On_Model()
+        {
+            var result = myForFun.Fare;
+
+            Assert.Equal(45.00, result);
+        }
+
+
+        [Fact]
+        public void ForFunConstructor_Sets_Location_On_Model()
+        {
+            var result = myForFun.Location;
+
+            Assert.Equal("Orlando", result);
+        }
+
+        [Fact]
+        public void ForFunConstructor_Sets_Name_On_Model()
+        {
+            var result = myForFun.Name;
+
+            Assert.Equal("Didny Worl", result);
+        }
+
+        [Fact]
+        public void ForFunConstructor_Sets_IsApproved__To_false()
+        {
+            var result = myForFun.IsApproved;
+
+            Assert.False(result);
+        }
+    }
+
+
+}
---
+++ BlueSkyTravel/BlueSkyTravel/Models/ForFun.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueSkyTravel.Models
+{
+    public class ForFun
+    {
+        public int Id { get; set; }
+        public DateTime EventTime { get; set; }
+        public double Fare { get; set; }
+        public bool IsApproved { get; set; }
+        public string Location { get; set; }
+        public string Name { get; set; }
+
+        public ForFun()
+        {
+
+        }
+
+        public ForFun(int id, DateTime eventTime, double fare, bool isApproved, string location, string name)
+        {
+            Id = id;
+            EventTime = eventTime;
+            Fare = fare;
+            IsApproved = false;
+            Location = location;
+            Name = name;
+        }
+
+
+    }
+}
