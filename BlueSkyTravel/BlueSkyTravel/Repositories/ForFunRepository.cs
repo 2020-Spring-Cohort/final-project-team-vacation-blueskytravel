2e4287b75f83c5e9a7f8ffedb4f2c3bcc46ae68c
joshuatreeee <joshuatreeee@gmail.com>
2020-03-31 13:26:49 -0400

    added repository classes for all models

---
+++ BlueSkyTravel/BlueSkyTravel/Repositories/ForFunRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueSkyTravel.Repositories
+{
+    public class ForFunRepository : Repository<ForFun>
+    {
+
+        public ForFunRepository(BlueSkyContext context) : base(context)
+        {
+                
+        }
+    }
+}
---
+++ BlueSkyTravel/BlueSkyTravel/Repositories/HotelRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlueSkyTravel.Models;
+
+namespace BlueSkyTravel.Repositories
+{
+    public class HotelRepository : Repository<Hotel>, IRepository<Hotel>
+    {
+        public HotelRepository(BlueSkyContext context) : base(context)
+        {
+
+        }
+    }
+}
