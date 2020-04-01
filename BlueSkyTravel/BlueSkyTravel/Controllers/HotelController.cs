89b55b4d83a46970f5743b041321644b5805b43b
joshuatreeee <joshuatreeee@gmail.com>
2020-04-01 14:45:51 -0400

    added crud actions for hotel

---
+++ BlueSkyTravel/BlueSkyTravel/Controllers/HotelController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlueSkyTravel.Controllers
+{
+    public class HotelController : Controller
+    {
+        IRepository<Hotel> hotelRepo;
+
+        public HotelController(IRepository<Hotel> hotelRepo)
+        {
+            this.h = itineraryRepo;
+        }
+        // GET: Hotel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Hotel/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Hotel/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Hotel/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Hotel/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Hotel/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Hotel/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Hotel/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
