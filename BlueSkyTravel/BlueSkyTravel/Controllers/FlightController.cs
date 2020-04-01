4817aa503c5ba9c25e61617aa8ef4752dca50b89
rhi-teetzel <rhiannateetzel@gmail.com>
2020-04-01 11:42:04 -0400

    Flight Controller set up

---
+++ BlueSkyTravel/BlueSkyTravel/Controllers/FlightController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlueSkyTravel.Models;
+using BlueSkyTravel.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlueSkyTravel.Controllers
+{
+    public class FlightController : Controller
+    {
+        IRepository<Flight> flightRepo;
+
+        public FlightController(IRepository<Flight> flightRepo)
+        {
+            this.flightRepo = flightRepo;
+        }
+        // GET: Flight
+        public ViewResult Index()
+        {
+            var model = flightRepo.GetAll();
+            return View(model);
+        }
+
+        // GET: Flight/Details/5
+        public ViewResult Details(int id)
+        {
+            var model = flightRepo.GetById(id);
+            return View(model);
+        }
+
+        // GET: Flight/Create
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        // POST: Flight/Create
+        [HttpPost]
+        public IActionResult Create(Flight flight)
+        {
+            flightRepo.Create(flight);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Flight/Edit/5
+        [HttpGet]
+        public ViewResult Update(int id)
+        {
+            Flight model = flightRepo.GetById(id);
+            return View(model);
+        }
+
+        // POST: Flight/Edit/5
+        [HttpPost]
+        public IActionResult Update(Flight flight)
+        {
+            flightRepo.Update(flight);
+            return RedirectToAction("Detail", "Flight", new { id = flight.Id });
+        }
+
+        // GET: Flight/Delete/5
+        [HttpGet]
+        public ViewResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Flight/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
