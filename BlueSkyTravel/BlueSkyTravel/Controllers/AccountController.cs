c1bbebbd0452675dd23542a13cb206a9ac81f612
TShaw516 <TShaw516@gmail.com>
2020-04-04 22:37:25 -0400

    built login IActionResults in controller

--- BlueSkyTravel/BlueSkyTravel/Controllers/AccountController.cs
+++ BlueSkyTravel/BlueSkyTravel/Controllers/AccountController.cs
@@ -52,6 +52,30 @@ namespace BlueSkyTravel.Controllers
         }
 
         [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+           
+            ModelState.AddModelError(string.Empty, "Login Failed!");
+               
+            }
+            return View(model);
+        }
+
+        [HttpGet]
         [AllowAnonymous]
         public async Task<IActionResult> Login(string returnUrl)
         {
