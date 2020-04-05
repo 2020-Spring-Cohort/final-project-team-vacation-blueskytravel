7b7ac8a49371c1f9f42049928ecc725c6db78065
TShaw516 <TShaw516@gmail.com>
2020-04-05 02:30:52 -0400

    setting up many to many relationship between users and roles

--- BlueSkyTravel/BlueSkyTravel/Controllers/AdministrationController.cs
+++ BlueSkyTravel/BlueSkyTravel/Controllers/AdministrationController.cs
@@ -116,5 +116,41 @@ namespace BlueSkyTravel.Controllers
             var roles = roleManager.Roles;
             return View(roles);
         }
+
+        public async Task<IActionResult> EditUsersInRole(string roleId)
+        {
+            ViewBag.roleId = roleId;
+            var role = await roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} cannot be found"
+                return View("NotFound");
+            }
+
+            var model = new List<UserRoleViewModel>();
+
+            foreach (var user in userManager.Users)
+            {
+                var userRoleViewModel = new UserRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName
+                };
+
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRoleViewModel.IsSelected = true;
+                }
+                else
+                {
+                    userRoleViewModel.IsSelected = false;
+                }
+
+                model.Add(userRoleViewModel);
+            }
+
+            return View(model);
+        }
     }
 }
