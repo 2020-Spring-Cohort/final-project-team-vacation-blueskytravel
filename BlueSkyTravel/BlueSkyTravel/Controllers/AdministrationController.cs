b512b27d8a5c2ad64f84202eaf07ed22f8c21261
TShaw516 <TShaw516@gmail.com>
2020-04-06 06:02:56 -0400

    verified functionality of rolesviews still need to enable the views from shared layout

--- BlueSkyTravel/BlueSkyTravel/Controllers/AdministrationController.cs
+++ BlueSkyTravel/BlueSkyTravel/Controllers/AdministrationController.cs
@@ -1,5 +1,6 @@
 ﻿using BlueSkyTravel.Areas.IdentityModel;
 using BlueSkyTravel.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace BlueSkyTravel.Controllers
 {
+    //[Authorize(Roles = "Administrator")]
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
