e01cc0c85ba410bafde60c8a79d39ffb35f854c4
dealer426 <burnssamuel01@gmail.com>
2020-04-07 10:32:41 -0400

    Added aboutus to controller

--- BlueSkyTravel/BlueSkyTravel/Controllers/HomeController.cs
+++ BlueSkyTravel/BlueSkyTravel/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace BlueSkyTravel.Controllers
         {
             return View();
         }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
     }
 }
