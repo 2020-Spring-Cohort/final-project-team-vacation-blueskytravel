f2c9b4fb6fb3670acbecb803a1e05a4c71db4777
joshuatreeee <joshuatreeee@gmail.com>
2020-04-13 17:37:34 -0400

    added remaining balance in javascript to top of carousel

--- BlueSkyTravel/BlueSkyTravel/wwwroot/js/site.js
+++ BlueSkyTravel/BlueSkyTravel/wwwroot/js/site.js
@@ -16,3 +16,29 @@ function addVote() {
 }
 
 
+let flightFareSum = 0
+let flightFares = document.querySelectorAll("#flightFare").forEach(flightFare => {
+    flightFareSum = flightFareSum + +flightFare.value
+})
+
+let hotelRateSum = 0
+let hotelRates = document.querySelectorAll("#hotelRate").forEach(hotelRate => {
+    hotelRateSum = hotelRateSum + +hotelRate.value
+})
+
+let activityFareSum = 0
+let activityFares = document.querySelectorAll("#activityFare").forEach(activityFare => {
+    activityFareSum = activityFareSum + +activityFare.value
+})
+
+console.log(flightFareSum)
+console.log(hotelRateSum)
+console.log(activityFareSum)
+
+let totalBudget = document.getElementById("budget").getAttribute('value')
+console.log(totalBudget)
+
+let totalBalanceElement = document.querySelector("#balance")
+let totalBalance = totalBudget - flightFareSum - hotelRateSum - activityFareSum
+totalBalanceElement.innerText = "Remaining Budget: $" + totalBalance.toFixed(2)
+console.log(totalBalance)
