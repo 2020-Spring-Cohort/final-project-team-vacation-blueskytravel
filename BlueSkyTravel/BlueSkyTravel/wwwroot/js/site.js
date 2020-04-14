4dc0cb23759ecf043edf49d3b4b8bbe0dbf539e1
rhi-teetzel <rhiannateetzel@gmail.com>
2020-04-14 10:38:49 -0400

    countdown implemented

--- BlueSkyTravel/BlueSkyTravel/wwwroot/js/site.js
+++ BlueSkyTravel/BlueSkyTravel/wwwroot/js/site.js
@@ -2,20 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-const button = document.querySelectorAll("#addVote").forEach(button => {
-    button.onclick = function () {
-        console.log("hello!")
-        addVote()
-    }
-})
-
-function addVote() {
-    let vote = document.querySelector("#voteCount").value
-    vote++
-    console.log(vote)
-}
 
 
+//Budget Balance Calculator
 let flightFareSum = 0
 let flightFares = document.querySelectorAll("#flightFare").forEach(flightFare => {
     flightFareSum = flightFareSum + +flightFare.value
@@ -42,3 +31,31 @@ let totalBalanceElement = document.querySelector("#balance")
 let totalBalance = totalBudget - flightFareSum - hotelRateSum - activityFareSum
 totalBalanceElement.innerText = "Remaining Budget: $" + totalBalance.toFixed(2)
 console.log(totalBalance)
+
+//Vacation Countdown
+const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+let countDown = new Date('Sep 30, 2020 00:00:00').getTime()
+
+    console.log(countDown)
+
+    x = setInterval(function () {
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById('days').innerText = Math.floor(distance / (day)),
+            document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        //if (distance < 0) {
+        //  clearInterval(x);
+        //  'IT'S MY BIRTHDAY!;
+        //}
+
+    }, second)
